a81d6d622ad8a4b71af75fd66affb3d30286efd9
chiayolin <chiayo.lin@gmail.com>
2017-02-06 11:18:18 +0700

    update git info

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,9 @@
 git clone --depth 1 --branch master --single-branch $DEPLOY_REPO .deploy 
 cd .deploy
 
+git config --global user.name 'HearingVietnam-bot'
+git config --global user.email 'hearingvietnam.bot@gmail.com'
+
 rm -rf ./*
 cp -r ../public/* .
 git add -A .
